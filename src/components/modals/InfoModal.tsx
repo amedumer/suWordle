149a1b4e9016eeb0ac85f4b6bf930c4057ea6f55
Ahmet Ömer Kayabaşı <69629296+amedumer@users.noreply.github.com>
2022-03-12 01:56:06 +0300

    initial commit

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,9 +8,15 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal
+      title="How to play suWordle"
+      isOpen={isOpen}
+      handleClose={handleClose}
+    >
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
+        Guess the word in 6 tries. The word is somehow related to Sabanci
+        University! It might be a well-known name, a place, an event, but its
+        always something about us. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
 
@@ -18,53 +24,52 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="A"
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="K" />
+        <Cell value="S" />
+        <Cell value="I" />
+        <Cell value="N" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter A is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
+        <Cell value="F" />
         <Cell value="I" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="N"
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="A" />
+        <Cell value="L" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter N is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
         <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="R" />
+        <Cell value="A" />
+        <Cell isRevealing={true} isCompleted={true} value="V" status="absent" />
         <Cell value="E" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter V is not in the word in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        Want to suggest new words? Submit your submission -{' '}
         <a
-          href="https://github.com/cwackerfuss/react-wordle"
+          href="https://forms.gle/wsz51djLVLTTw4XdA"
           className="underline font-bold"
         >
-          check out the code here
+          from here
         </a>{' '}
       </p>
     </BaseModal>
